1c72dcf33db307c8abed16ae6e6fdb0620a8d720
Dirk Stolle <striezel-dev@web.de>
2020-11-08 21:03:40 +0100

    extend test case for JSON parsing of sync requests

--- tests/json/Sync.cpp
+++ tests/json/Sync.cpp
@@ -99,4 +99,172 @@ TEST_CASE("parsing sync events")
     REQUIRE( std::find(invitedRoomIds.begin(), invitedRoomIds.end(), "!next0r:ma.tricks.example.tld") != invitedRoomIds.end() );
   }
 
+  SECTION("messages in rooms")
+  {
+    const std::string json = R"json(
+    {
+    "next_batch":"foobar1234_5678_9012",
+    "rooms":{
+        "join":{
+            "!roomid1234:example.com":{
+                "summary":{
+                    "m.heroes":[
+                        "@alice:example.com",
+                        "@bob:example.com"
+                    ],
+                    "m.joined_member_count":2,
+                    "m.invited_member_count":0
+                },
+                "state":{
+                    "events":[
+                        {
+                            "content":{
+                                "membership":"join",
+                                "avatar_url":"mxc://example.org/SEsfnsuifSDFSSEF",
+                                "displayname":"Unit Tests are Good"
+                            },
+                            "type":"m.room.member",
+                            "event_id":"$ashdbasjdhauuuuau:example.org",
+                            "room_id":"!roomid1234:example.com",
+                            "sender":"@example:example.org",
+                            "origin_server_ts":12345678901234,
+                            "unsigned":{
+                                "age":1234
+                            },
+                            "state_key":"@alice:example.org"
+                        }
+                    ]
+                },
+                "timeline":{
+                    "events":[
+                        {
+                            "content":{
+                                "membership":"join",
+                                "avatar_url":"mxc://example.org/SEsfnsuifSDFSSEF",
+                                "displayname":"Unit Tests are Good"
+                            },
+                            "type":"m.room.member",
+                            "event_id":"$ashdbasjdhauuuuau:example.org",
+                            "room_id":"!roomid1234:example.com",
+                            "sender":"@example:example.org",
+                            "origin_server_ts":12345678901234,
+                            "unsigned":{
+                                "age":1234
+                            },
+                            "state_key":"@alice:example.org"
+                        },
+                        {
+                            "content":{
+                                "body":"This is an example text message",
+                                "msgtype":"m.text",
+                                "format":"org.matrix.custom.html",
+                                "formatted_body":"<b>This is an example text message</b>"
+                            },
+                            "type":"m.room.message",
+                            "event_id":"$aaaaaaaaaaaaaaaaa:example.org",
+                            "room_id":"!roomid1234:example.com",
+                            "sender":"@example:example.org",
+                            "origin_server_ts":13375678901234,
+                            "unsigned":{
+                                "age":1234
+                            }
+                        }
+                    ],
+                    "limited":true,
+                    "prev_batch":"p12-123456_0_0"
+                },
+                "ephemeral":{
+                    "events":[
+                        {
+                            "content":{
+                                "user_ids":[
+                                    "@alice:matrix.org",
+                                    "@bob:example.com"
+                                ]
+                            },
+                            "type":"m.typing",
+                            "room_id":"!09876abcde:example.org"
+                        }
+                    ]
+                }
+            },
+            "!roomid5678:example.com":{
+               "timeline":{
+                    "events":[
+                        {
+                            "content":{
+                                "body":"Message without formatted body",
+                                "msgtype":"m.text"
+                            },
+                            "type":"m.room.message",
+                            "event_id":"$aaaaaaaaaaaaaaaaa:example.org",
+                            "room_id":"!roomid1234:example.com",
+                            "sender":"@pacman:games.tld",
+                            "origin_server_ts":1234567654321,
+                            "unsigned":{
+                                "age":1234
+                            }
+                        },
+                        {
+                            "content":{
+                                "body":"Another text message",
+                                "msgtype":"m.text",
+                                "format":"org.matrix.custom.html",
+                                "formatted_body":"<b>Another</b> text message"
+                            },
+                            "type":"m.room.message",
+                            "event_id":"$aaaaaaaaaaaaaaaaa:example.org",
+                            "room_id":"!roomid1234:example.com",
+                            "sender":"@sending_user_id:example.org",
+                            "origin_server_ts":1223344554321,
+                            "unsigned":{
+                                "age":1234
+                            }
+                        }
+                    ],
+                    "limited":true,
+                    "prev_batch":"p12-123456_0_0"
+                }
+            }
+        },
+        "invite":{ }
+    }
+    }
+    )json";
+    const auto [doc, error] = parser.parse(json);
+    REQUIRE_FALSE( error );
+    Sync::parse(doc, rooms, invitedRoomIds);
+
+    // Invited rooms should be empty (no data in invite element).
+    REQUIRE( invitedRoomIds.size() == 0 );
+    // Rooms should be two.
+    REQUIRE( rooms.size() == 2 );
+    // Id of 1st room should be "!roomid1234:example.com".
+    REQUIRE( "!roomid1234:example.com" == rooms[0].id );
+    // First room should have one text message.
+    REQUIRE( rooms[0].texts.size() == 1 );
+    // Check contents of 1st message in 1st room.
+    REQUIRE( rooms[0].texts[0].body == "This is an example text message" );
+    REQUIRE( rooms[0].texts[0].format == "org.matrix.custom.html" );
+    REQUIRE( rooms[0].texts[0].formatted_body == "<b>This is an example text message</b>" );
+    REQUIRE( rooms[0].texts[0].sender == "@example:example.org" );
+    REQUIRE( rooms[0].texts[0].server_ts == std::chrono::milliseconds(13375678901234) );
+    // Id of 2nd room should be "!roomid5678:example.com".
+    REQUIRE( "!roomid5678:example.com" == rooms[1].id );
+    // Second room should have two text messages.
+    REQUIRE( rooms[1].texts.size() == 2 );
+    // Check contents of 1st message in 2nd room.
+    REQUIRE( rooms[1].texts[0].body == "Message without formatted body" );
+    REQUIRE( rooms[1].texts[0].format.empty() );
+    REQUIRE( rooms[1].texts[0].formatted_body.empty() );
+    REQUIRE( rooms[1].texts[0].sender == "@pacman:games.tld" );
+    REQUIRE( rooms[1].texts[0].server_ts == std::chrono::milliseconds(1234567654321) );
+    // Check contents of 2nd message in 2nd room.
+    REQUIRE( rooms[1].texts[1].body == "Another text message" );
+    REQUIRE( rooms[1].texts[1].format == "org.matrix.custom.html" );
+    REQUIRE( rooms[1].texts[1].formatted_body == "<b>Another</b> text message" );
+    REQUIRE( rooms[1].texts[1].sender == "@sending_user_id:example.org" );
+    REQUIRE( rooms[1].texts[1].server_ts == std::chrono::milliseconds(1223344554321) );
+  }
+
 }
