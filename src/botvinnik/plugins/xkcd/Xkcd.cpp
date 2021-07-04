92e5144123427c37ad2e6c0a469b80daebfc426f
Dirk Stolle <striezel-dev@web.de>
2021-07-04 18:48:40 +0200

    simplify code structure for handling of xkcd command

--- src/botvinnik/plugins/xkcd/Xkcd.cpp
+++ src/botvinnik/plugins/xkcd/Xkcd.cpp
@@ -54,104 +54,104 @@ unsigned int Xkcd::getRandomNumber() const
 
 Message Xkcd::handleCommand(const std::string_view& command, const std::string_view& message, const std::string_view& userId, const std::string_view& roomId, const std::chrono::milliseconds& server_ts)
 {
-  if (command == "xkcd")
+  if (command != "xkcd")
   {
-    unsigned int num = 0;
-    if (message.size() > command.size() + 1)
-    {
-      std::string number(message.substr(command.size()));
-      trim(number);
-      unsigned long value;
-      std::size_t pos = 0;
-      try
-      {
-        value = std::stoul(number, &pos);
-        if (pos == number.size() && value > 0 && value <= mLatestNum)
-        {
-           num = value;
-        }
-      }
-      catch (const std::exception& ex)
-      {
-        // Nothing to do here.
-      }
-    }
-
-    if (num == 0)
-      num = getRandomNumber();
-
-    // XkcdData::get() only works when server can be reached.
-    const auto info = XkcdData::get(num);
-    if (!info.has_value() || info.value().img.empty())
-    {
-      return Message("Error: Could not get comic from xkcd.com!",
-                     std::string("<strong>Error:</strong> Could not get comic from xkcd.com!"));
-    }
-    const XkcdData& data = info.value();
-    auto db = XkcdDb::getDatabase();
-    std::optional<std::string> mxcUri;
-    if (db.has_value())
-    {
-      mxcUri = XkcdDb::getMxcUri(db.value(), data.num);
-      if (!mxcUri.has_value())
-      {
-        // No existing media. Upload it and save it in DB.
-        mxcUri = theMatrix.uploadImage(data.img);
-        if (mxcUri.has_value())
-        {
-          if (XkcdDb::insertMxcUri(db.value(), data.num, mxcUri.value()))
-          {
-            std::clog << "Info: Inserted MXC URI for comic #" << data.num << " into database." << std::endl;
-          }
-          else
-          {
-            std::clog << "Warning: Failed to insert MXC URI for comic #" << data.num << " into database!" << std::endl;
-          }
-        }
-      }
-    }
-    else
-    {
-      // Database error occurred or DB does not exist. Fallback to old approach
-      // and upload new media every time.
-      mxcUri = theMatrix.uploadImage(data.img);
-    }
-
-    Message xkcd;
-    // Create normal body - varies, when transcript is there.
-    xkcd.body = "xkcd.com # " + std::to_string(data.num) + ": " + data.title
-              + "\n\n";
-    if (!data.transcript.empty())
-    {
-      xkcd.body.append(data.transcript).append("\n\n");
-    }
-    else
-    {
-      xkcd.body.append(data.img).append("\n\n").append(data.alt).append("\n\n");
-    }
-    xkcd.body.append("Source: https://xkcd.com/" + std::to_string(data.num) + "/");
-    // formatted body
-    if (mxcUri.has_value())
-    {
-      // Use mxc URI for image.
-      xkcd.formatted_body = "xkcd.com # " + std::to_string(data.num) + ": <strong>" + data.title
-              + "</strong><br />\n<br />\n<img src=\"" + mxcUri.value() + "\" alt=\"\"><br >\n"
-              + "<br /><em>" + data.alt + "</em><br />Source: https://xkcd.com/"
-              + std::to_string(data.num) + "/";
-    }
-    else
-    {
-      // Upload failed. Use original URL as fallback.
-      xkcd.formatted_body = "xkcd.com # " + std::to_string(data.num) + ": <strong>" + data.title
-              + "</strong><br />\n<br />\n" + data.img + "<br >\n"
-              + "<br /><em>" + data.alt + "</em><br />Source: https://xkcd.com/"
-              + std::to_string(data.num) + "/";
-    }
-    return xkcd;
+    // unknown command
+    return Message();
   }
 
-  // unknown command
-  return Message();
+  unsigned int num = 0;
+  if (message.size() > command.size() + 1)
+  {
+    std::string number(message.substr(command.size()));
+    trim(number);
+    unsigned long value;
+    std::size_t pos = 0;
+    try
+    {
+      value = std::stoul(number, &pos);
+      if (pos == number.size() && value > 0 && value <= mLatestNum)
+      {
+         num = value;
+      }
+    }
+    catch (const std::exception& ex)
+    {
+      // Nothing to do here.
+    }
+  }
+
+  if (num == 0)
+    num = getRandomNumber();
+
+  // XkcdData::get() only works when server can be reached.
+  const auto info = XkcdData::get(num);
+  if (!info.has_value() || info.value().img.empty())
+  {
+    return Message("Error: Could not get comic from xkcd.com!",
+                   std::string("<strong>Error:</strong> Could not get comic from xkcd.com!"));
+  }
+  const XkcdData& data = info.value();
+  auto db = XkcdDb::getDatabase();
+  std::optional<std::string> mxcUri;
+  if (db.has_value())
+  {
+    mxcUri = XkcdDb::getMxcUri(db.value(), data.num);
+    if (!mxcUri.has_value())
+    {
+      // No existing media. Upload it and save it in DB.
+      mxcUri = theMatrix.uploadImage(data.img);
+      if (mxcUri.has_value())
+      {
+        if (XkcdDb::insertMxcUri(db.value(), data.num, mxcUri.value()))
+        {
+          std::clog << "Info: Inserted MXC URI for comic #" << data.num << " into database." << std::endl;
+        }
+        else
+        {
+          std::clog << "Warning: Failed to insert MXC URI for comic #" << data.num << " into database!" << std::endl;
+        }
+      }
+    }
+  }
+  else
+  {
+    // Database error occurred or DB does not exist. Fallback to old approach
+    // and upload new media every time.
+    mxcUri = theMatrix.uploadImage(data.img);
+  }
+
+  Message xkcd;
+  // Create normal body - varies, when transcript is there.
+  xkcd.body = "xkcd.com # " + std::to_string(data.num) + ": " + data.title
+            + "\n\n";
+  if (!data.transcript.empty())
+  {
+    xkcd.body.append(data.transcript).append("\n\n");
+  }
+  else
+  {
+    xkcd.body.append(data.img).append("\n\n").append(data.alt).append("\n\n");
+  }
+  xkcd.body.append("Source: https://xkcd.com/" + std::to_string(data.num) + "/");
+  // formatted body
+  if (mxcUri.has_value())
+  {
+    // Use mxc URI for image.
+    xkcd.formatted_body = "xkcd.com # " + std::to_string(data.num) + ": <strong>" + data.title
+            + "</strong><br />\n<br />\n<img src=\"" + mxcUri.value() + "\" alt=\"\"><br >\n"
+            + "<br /><em>" + data.alt + "</em><br />Source: https://xkcd.com/"
+            + std::to_string(data.num) + "/";
+  }
+  else
+  {
+    // Upload failed. Use original URL as fallback.
+    xkcd.formatted_body = "xkcd.com # " + std::to_string(data.num) + ": <strong>" + data.title
+            + "</strong><br />\n<br />\n" + data.img + "<br >\n"
+            + "<br /><em>" + data.alt + "</em><br />Source: https://xkcd.com/"
+            + std::to_string(data.num) + "/";
+  }
+  return xkcd;
 }
 
 std::string Xkcd::helpOneLine(const std::string_view& command) const
