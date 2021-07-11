da13202089154bc2d4a4724a9eeaa09beb8b7602
Dirk Stolle <striezel-dev@web.de>
2021-07-11 19:11:35 +0200

    bump version to 0.3.7

--- src/Version.hpp
+++ src/Version.hpp
@@ -27,14 +27,14 @@ namespace bvn
 {
 
 /** \brief version information */
-const std::string version = "version 0.3.6, 2021-07-11";
+const std::string version = "version 0.3.7, 2021-07-11";
 
 #ifdef BVN_USER_AGENT
 /** \brief the User-Agent to use when sending requests to the Matrix server
  *
  * \remark User-Agent is disabled by default.
  */
-const std::string userAgent = "botvinnik/0.3.6";
+const std::string userAgent = "botvinnik/0.3.7";
 #endif
 
 } // namespace
