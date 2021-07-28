30c30030d066c4cc11a45d2dc69c982f1b209986
Dirk Stolle <striezel-dev@web.de>
2021-07-28 22:51:43 +0200

    bump version to 0.3.10

--- changelog.md
+++ changelog.md
@@ -4,7 +4,7 @@ _(Note: This changelog focuses on the major changes between the different
 versions. Therefore, it may not contain all changes. Especially smaller fixes or
 improvements may be omitted.)_
 
-## Version 0.?.? (2021-07-??)
+## Version 0.3.10 (2021-07-28)
 
 An HTML injection vulnerability in the implementation of the `!cheats` command
 is fixed.
--- src/Version.hpp
+++ src/Version.hpp
@@ -27,14 +27,14 @@ namespace bvn
 {
 
 /** \brief version information */
-const std::string version = "version 0.3.9, 2021-07-15";
+const std::string version = "version 0.3.10, 2021-07-28";
 
 #ifdef BVN_USER_AGENT
 /** \brief the User-Agent to use when sending requests to the Matrix server
  *
  * \remark User-Agent is disabled by default.
  */
-const std::string userAgent = "botvinnik/0.3.9";
+const std::string userAgent = "botvinnik/0.3.10";
 #endif
 
 } // namespace
