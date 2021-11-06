3d7e24706a539fdd8624a18ecf79c5a2ce4ba535
Dirk Stolle <striezel-dev@web.de>
2021-11-06 11:44:42 +0100

    bump version to 0.3.13

--- changelog.md
+++ changelog.md
@@ -4,7 +4,7 @@ _(Note: This changelog focuses on the major changes between the different
 versions. Therefore, it may not contain all changes. Especially smaller fixes or
 improvements may be omitted.)_
 
-## Version 0.?.? (2021-10-??)
+## Version 0.3.13 (2021-11-06)
 
 The `!corona` command switches to canonical URL of disease.sh API for its
 requests. The plugin does now use `https://disease.sh/` as base URL for API
--- src/Version.hpp
+++ src/Version.hpp
@@ -27,14 +27,14 @@ namespace bvn
 {
 
 /** \brief version information */
-const std::string version = "version 0.3.12, 2021-10-01";
+const std::string version = "version 0.3.13, 2021-11-06";
 
 #ifdef BVN_USER_AGENT
 /** \brief the User-Agent to use when sending requests to the Matrix server
  *
  * \remark User-Agent is disabled by default.
  */
-const std::string userAgent = "botvinnik/0.3.12";
+const std::string userAgent = "botvinnik/0.3.13";
 #endif
 
 } // namespace
