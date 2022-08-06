0c00d9763caf2c5e4fefa0cd43568ece3eea2be4
Dirk Stolle <striezel-dev@web.de>
2022-08-06 02:02:23 +0200

    bump version to 0.4.1

--- changelog.md
+++ changelog.md
@@ -4,7 +4,7 @@ _(Note: This changelog focuses on the major changes between the different
 versions. Therefore, it may not contain all changes. Especially smaller fixes or
 improvements may be omitted.)_
 
-## Version 0.?.? (2022-05-??)
+## Version 0.4.1 (2022-08-06)
 
 The library that does the JSON parsing (simdjson) has been updated from version
 1.0.2 to version 2.2.2.
--- src/Version.hpp
+++ src/Version.hpp
@@ -27,14 +27,14 @@ namespace bvn
 {
 
 /** \brief version information */
-const std::string version = "version 0.4.0, 2022-05-03";
+const std::string version = "version 0.4.1, 2022-08-06";
 
 #ifdef BVN_USER_AGENT
 /** \brief the User-Agent to use when sending requests to the Matrix server
  *
  * \remark User-Agent is disabled by default.
  */
-const std::string userAgent = "botvinnik/0.4.0";
+const std::string userAgent = "botvinnik/0.4.1";
 #endif
 
 } // namespace
