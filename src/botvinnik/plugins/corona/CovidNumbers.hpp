cdd0d4be7d44a6278402d82c0eb90a4e48492310
Dirk Stolle <striezel-dev@web.de>
2023-06-15 21:33:08 +0200

    add missing #include directive

--- src/botvinnik/plugins/corona/CovidNumbers.hpp
+++ src/botvinnik/plugins/corona/CovidNumbers.hpp
@@ -1,7 +1,7 @@
 /*
  -------------------------------------------------------------------------------
     This file is part of the botvinnik Matrix bot.
-    Copyright (C) 2020, 2021  Dirk Stolle
+    Copyright (C) 2020, 2021, 2023  Dirk Stolle
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -21,6 +21,7 @@
 #ifndef BVN_PLUGIN_COVIDNUMBERS_HPP
 #define BVN_PLUGIN_COVIDNUMBERS_HPP
 
+#include <cstdint>
 #include <string>
 #include <vector>
 
