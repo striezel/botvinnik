a6cbd591cd0f1b9f38c257e1e55dccf79b2254d1
Dirk Stolle <striezel-dev@web.de>
2023-12-03 23:51:23 +0100

    tests: add test for ImageInfo structure

---
+++ tests/components/matrix/ImageInfo.cpp
@@ -0,0 +1,36 @@
+/*
+ -------------------------------------------------------------------------------
+    This file is part of the test suite for botvinnik.
+    Copyright (C) 2023  Dirk Stolle
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------------------
+*/
+
+#include "../../locate_catch.hpp"
+#include "../../../src/matrix/ImageInfo.hpp"
+
+TEST_CASE("ImageInfo constructor")
+{
+  using namespace bvn;
+
+  SECTION("defaults must be set")
+  {
+    const ImageInfo info;
+    REQUIRE( info.mimeType.empty() );
+    REQUIRE( 0 == info.width );
+    REQUIRE( 0 == info.height );
+    REQUIRE( 0 == info.size );
+  }
+}
