ab7c4fcabc28259171e469dacb3c7b824cafc9f8
Dirk Stolle <striezel-dev@web.de>
2023-12-03 23:41:39 +0100

    move some test files to reflect source directory structure

--- tests/components/matrix/events/PowerLevels.cpp
+++ tests/components/matrix/events/PowerLevels.cpp
@@ -1,7 +1,7 @@
 /*
  -------------------------------------------------------------------------------
     This file is part of the test suite for botvinnik.
-    Copyright (C) 2020  Dirk Stolle
+    Copyright (C) 2020, 2023  Dirk Stolle
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -18,8 +18,8 @@
  -------------------------------------------------------------------------------
 */
 
-#include "../locate_catch.hpp"
-#include "../../src/matrix/events/PowerLevels.hpp"
+#include "../../../locate_catch.hpp"
+#include "../../../../src/matrix/events/PowerLevels.hpp"
 
 TEST_CASE("PowerLevels constructor")
 {
