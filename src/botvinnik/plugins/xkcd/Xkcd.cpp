03e2053a78b5bb3d154d24fb5a7863b860092cb8
Dirk Stolle <striezel-dev@web.de>
2023-12-10 00:50:16 +0100

    update fallback value for xkcd command

--- src/botvinnik/plugins/xkcd/Xkcd.cpp
+++ src/botvinnik/plugins/xkcd/Xkcd.cpp
@@ -29,7 +29,7 @@ namespace bvn
 {
 
 Xkcd::Xkcd(Matrix& mat)
-: mLatestNum(2706),
+: mLatestNum(2865),
   mLastUpdate(std::chrono::steady_clock::now() - std::chrono::hours(24)),
   theMatrix(mat)
 {
